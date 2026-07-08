130a8f8e1a7c65855eafef816e8bafd93e3bdf4f
markperezne8-spec <markperezne8@gmail.com>
2026-07-08 18:47:19 -0400

    feat: install listing plan queue in desktop launcher

--- launcher.py
+++ launcher.py
@@ -10,6 +10,7 @@ from ui.inventory_sale_readiness_feature import install_inventory_sale_readiness
 from ui.inventory_profit_feature import install_inventory_profit_feature
 from ui.inventory_price_guidance_feature import install_inventory_price_guidance_feature
 from ui.inventory_listing_workspace_feature import install_inventory_listing_workspace_feature
+from ui.inventory_listing_plan_queue_feature import install_inventory_listing_plan_queue_feature
 
 
 if __name__ == '__main__':
@@ -24,5 +25,6 @@ if __name__ == '__main__':
     install_inventory_profit_feature(window)
     install_inventory_price_guidance_feature(window)
     install_inventory_listing_workspace_feature(window)
+    install_inventory_listing_plan_queue_feature(window)
     window.show()
     sys.exit(app.exec())
